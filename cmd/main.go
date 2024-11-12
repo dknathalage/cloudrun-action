514446adee6905c7b1d59ff8e80dd30d62d48d71
dknathalage <dknathalage@gmail.com>
2024-11-12 19:23:42 +1100

    test action

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,4 @@
 package main
 
-import (
-	"github.com/caarlos0/env"
-)
-
-type CloudRunService struct {
-	Name    string `env:"SERVICE,required"`
-	Region  string `env:"REGION,required"`
-	Project string `env:"PROJECT,required"`
-}
-
 func main() {
-	var cfg CloudRunService
-	err := env.Parse(&cfg)
-	if err != nil {
-		panic(err)
-	}
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/dknathalage/cloudrun-actions
 
 go 1.23.1
-
-require github.com/caarlos0/env v3.5.0+incompatible
-
-require github.com/stretchr/testify v1.9.0 // indirect
